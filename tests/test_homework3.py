fc9f75cf483b9246bc5a10b60090b6c94b64a021
An <anahitaleqs89@gmail.com>
2024-03-05 13:32:26 -0800

    homework3

---
+++ tests/test_homework3.py
@@ -0,0 +1,25 @@
+import time
+
+from playwright.sync_api import Page,expect
+from time import perf_counter
+
+def test_subscribe(page:Page):
+    page.goto(url="http://165.227.132.1/login")
+    sign_up= page.locator('a[Class="text-primary"]')
+    first_name=page.locator('//*[@name="firstName"]')
+    last_name=page.locator('//*[@name="lastName"]')
+    user_name=page.locator('//*[@name="username"]')
+    email=page.locator('//*[@name="email"]')
+    password=page.locator('//*[@name="password"]')
+    re_enter_password=page.locator('//*[@name="retype"]')
+    create_account=page.locator('//*[@type="submit"]')
+    sign_up.click()
+    first_name.fill("Anahit")
+    last_name.fill("Aleksanyan")
+    user_name.fill("Analeqs")
+    email.fill("anahitaleqs89@gmail.com")
+    password.fill("Ashun2024!")
+    re_enter_password.fill("Ashun2024!")
+    time.sleep(2)
+    create_account.click()
+    time.sleep(10)
