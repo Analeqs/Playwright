5dcb7bc98f75ffc3ef758e2bed550307069a6deb
An <anahitaleqs89@gmail.com>
2024-04-01 18:05:12 -0700

    done

--- tests/test_addpicture.py
+++ tests/test_addpicture.py
@@ -1,23 +1,23 @@
 from playwright.sync_api import Page, expect
 import time
+from pages.login_page import LoginPage
 
 
-def test_login(page:Page):
+def test_login(page :Page):
     page.goto(url="http://165.227.132.1/login")
-    email = page.locator('//*[@type="email"]')
-    password=page.locator('//*[@type="password"]')
-    sign_in = page.locator('//*[@type="submit"]')
-    avatar= page.locator('//a[@class="flex items-center gap-4"]//*[name()="svg"]')
-    profile=page.locator('//a[normalize-space()="My Profile"]')
-    add_pic=page.locator('//label[@for="profile"]//*[name()="svg"]')
-    email.fill("anahitaleqs89@gmail.com")
-    password.fill("Ashun2024!")
-    sign_in.click()
+    login_page = LoginPage(page)
+    login_page.fill_email("anahitaleqs89@gmail.com")
+    login_page.fill_password("Ashun2024!")
+    login_page.sigh_in()
+
+    avatar = page.locator('//a[@class="flex items-center gap-4"]//*[name()="svg"]')
+    profile = page.locator('//a[normalize-space()="My Profile"]')
+    add_pic = page.locator('//label[@for="profile"]//*[name()="svg"]')
     avatar.click()
     profile.click()
     add_pic.click()
     input_selector = 'input[type="file"]'
-    file_path = 'C:\\Users\\anahi\\Downloads\\test.jpg'
+    file_path = 'test.jpg'
     page.input_file(input_selector, file_path)
 
     time.sleep(10)
